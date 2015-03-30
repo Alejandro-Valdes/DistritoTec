6eee24f88e63a4ebaab34c0070fa0c17c611f247
eduardolozano1993 <eduardolozano1993@gmail.com>
2015-03-30 13:08:58 -0600

    documentation

--- Androides_PROYECTO_DistritoTec/app/src/main/java/itesm/mx/androides_proyecto_distritotec/MenuOpcionesTransporte/OpcionesTransporteProvider.java
+++ Androides_PROYECTO_DistritoTec/app/src/main/java/itesm/mx/androides_proyecto_distritotec/MenuOpcionesTransporte/OpcionesTransporteProvider.java
@@ -1,34 +1,50 @@
 package itesm.mx.androides_proyecto_distritotec.MenuOpcionesTransporte;
 
-import android.content.Context;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 /**
- * Created by Alejandro Valdes on 28-Mar-15.
+ * OpcionTransporteProvider
+ *
+ * Clase que provee la lista de las rutas disponibles
+ *
+ * @author Jose Eduardo Elizondo Lozano A01089591
+ * @author Oliver Alejandro Martínez Quiroz A01280416
+ * @author Jesús Alejandro Valdés Valdés A0099044
+ *
+ * Version 1.0
+ *
  */
 public class OpcionesTransporteProvider {
 
-
+    /**
+     * getInfo
+     *
+     * @param liFavs
+     * @return un valor <codeHashMap<String, List<String >></code> la lista de las rutas disponibles
+     */
     public static HashMap<String, List<String >> getInfo(List<String> liFavs){
 
         HashMap<String, List<String>> hmOpcionesTransporte = new HashMap<String, List<String>>();
 
+        // Lista del Expreso TEC
         List<String> liExpreso = new ArrayList<String>();
         liExpreso.add("Valle");
         liExpreso.add("Galerias");
         liExpreso.add("San Nicolas");
 
+        // Lista del Circuito TEC
         List<String> liCircuito = new ArrayList<String>();
         liCircuito.add("Villas TEC");
         liCircuito.add("Colonia Roma");
         liCircuito.add("Garza Sada");
 
+        // Nombre de las listas padres
         hmOpcionesTransporte.put("   Expreso", liExpreso);
         hmOpcionesTransporte.put("   Circuito", liCircuito);
 
+        // Lista de favoritos
         if(liFavs!=null){
             hmOpcionesTransporte.put("  Favoritos", liFavs);
         }
