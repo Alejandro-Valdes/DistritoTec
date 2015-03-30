8a2004805946c3f66ab6f3d57f8b931f8ea7deaa
eduardolozano1993 <eduardolozano1993@gmail.com>
2015-03-30 13:03:24 -0600

    documentation

--- Androides_PROYECTO_DistritoTec/app/src/main/java/itesm/mx/androides_proyecto_distritotec/MenuOpcionesTransporte/OpcionesTransporteAdapter.java
+++ Androides_PROYECTO_DistritoTec/app/src/main/java/itesm/mx/androides_proyecto_distritotec/MenuOpcionesTransporte/OpcionesTransporteAdapter.java
@@ -13,14 +13,32 @@ import java.util.List;
 import itesm.mx.androides_proyecto_distritotec.R;
 
 /**
- * Created by Alejandro Valdes on 28-Mar-15.
+ * OpcionTransporteAdapter
+ *
+ * Clase adaptador de las opciones de transporte, obtiene los elementos de la ListView
+ *
+ * @author Jose Eduardo Elizondo Lozano A01089591
+ * @author Oliver Alejandro Martínez Quiroz A01280416
+ * @author Jesús Alejandro Valdés Valdés A0099044
+ *
+ * Version 1.0
+ *
  */
 public class OpcionesTransporteAdapter extends BaseExpandableListAdapter{
 
-    private Context context;
-    private HashMap<String, List<String>> hmOpciones;
-    private List<String> liOpciones;
+    private Context context; // Contexto de la aplicacion
+    private HashMap<String, List<String>> hmOpciones; // Mapeo de la lista de rutas
+    private List<String> liOpciones; // Lista de rutas
 
+    /**
+     * OpcionesTransporteAdapter
+     *
+     * Metodo constructor de la clase OpcionesTransporteAdapter
+     *
+     * @param context
+     * @param hmOpciones
+     * @param liOpciones
+     */
     public OpcionesTransporteAdapter(Context context, HashMap<String, List<String>> hmOpciones,
                                      List<String> liOpciones){
         this.context = context;
@@ -28,41 +46,91 @@ public class OpcionesTransporteAdapter extends BaseExpandableListAdapter{
         this.liOpciones = liOpciones;
     }
 
+    /**
+     * getGroupCount
+     *
+     * @return un valor <code>int</code> regresa el tamaño de la lista
+     */
     @Override
     public int getGroupCount() {
         return liOpciones.size();
     }
 
+    /**
+     * getChildenCount
+     *
+     * @param groupPosition
+     * @return un valor <code>int</code> regresa la cantidad de hijos de la list view padre
+     */
     @Override
     public int getChildrenCount(int groupPosition) {
         return hmOpciones.get(liOpciones.get(groupPosition)).size();
     }
 
+    /**
+     * getGroup
+     *
+     * @param groupPosition
+     * @return un valor <code>Object</code> regresa la ListView padre
+     */
     @Override
     public Object getGroup(int groupPosition) {
         return liOpciones.get(groupPosition);
     }
 
+    /**
+     * getChild
+     *
+     * @param groupPosition
+     * @param childPosition
+     * @return un valor <code>Object</code> regresa el hijo selecionado
+     */
     @Override
     public Object getChild(int groupPosition, int childPosition) {
         return hmOpciones.get(liOpciones.get(groupPosition)).get(childPosition);
     }
 
+    /**
+     * getGroupId
+     *
+     * @param groupPosition
+     * @return un valor <code>long</code> regresa la posicion del padre
+     */
     @Override
     public long getGroupId(int groupPosition) {
         return groupPosition;
     }
 
+    /**
+     *
+     * @param groupPosition
+     * @param childPosition
+     * @return un valor <code>long</code> regresa la posicion del hijo
+     */
     @Override
     public long getChildId(int groupPosition, int childPosition) {
         return childPosition;
     }
 
+    /**
+     * hasStableIds
+     *
+     * @return un valor <code>boolean</code> falso
+     */
     @Override
     public boolean hasStableIds() {
         return false;
     }
 
+    /**
+     * getGroupView
+     *
+     * @param groupPosition
+     * @param isExpanded
+     * @param convertView
+     * @param parent
+     * @return un valor <code>View</code> regresa la vista del padre
+     */
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
 
@@ -78,6 +146,16 @@ public class OpcionesTransporteAdapter extends BaseExpandableListAdapter{
         return convertView;
     }
 
+    /**
+     * getChildView
+     *
+     * @param groupPosition
+     * @param childPosition
+     * @param isLastChild
+     * @param convertView
+     * @param parent
+     * @return un valor <code>View</code> regresa la vista del hijo
+     */
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
         String strChildName = (String) getChild(groupPosition,childPosition);
@@ -91,6 +169,13 @@ public class OpcionesTransporteAdapter extends BaseExpandableListAdapter{
         return convertView;
     }
 
+    /**
+     * isChildDelectable
+     *
+     * @param groupPosition
+     * @param childPosition
+     * @return un valor <code>boolean</code> true
+     */
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
         return true;
