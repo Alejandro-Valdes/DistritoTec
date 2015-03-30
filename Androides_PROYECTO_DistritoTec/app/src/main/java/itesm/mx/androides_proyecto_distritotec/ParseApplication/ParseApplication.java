bbc4c6614bf957553c92448903a4fbf48bdd74ec
eduardolozano1993 <eduardolozano1993@gmail.com>
2015-03-30 13:54:31 -0600

    documentation

--- Androides_PROYECTO_DistritoTec/app/src/main/java/itesm/mx/androides_proyecto_distritotec/ParseApplication/ParseApplication.java
+++ Androides_PROYECTO_DistritoTec/app/src/main/java/itesm/mx/androides_proyecto_distritotec/ParseApplication/ParseApplication.java
@@ -1,29 +1,39 @@
 package itesm.mx.androides_proyecto_distritotec.ParseApplication;
 
-/**
- * Created by Alejandro Valdes on 28-Mar-15.
- */
-
-/**
- * Clase que sirve para verificar nuestra app dentro de parse
- */
-
-
 import com.parse.Parse;
 import com.parse.ParseACL;
 import com.parse.ParseUser;
 import android.app.Application;
 
+/**
+ * ParseApplication
+ *
+ * Clase que se encarga de Parsear los datos
+ *
+ * @author Jose Eduardo Elizondo Lozano A01089591
+ * @author Oliver Alejandro Martínez Quiroz A01280416
+ * @author Jesús Alejandro Valdés Valdés A0099044
+ *
+ * Version 1.0
+ *
+ */
 public class ParseApplication extends Application {
 
+    /**
+     * onCreate
+     *
+     * Metodo que crea la aplicacion Parse
+     *
+     * @return void
+     */
     @Override
     public void onCreate(){
         super.onCreate();
 
-        //codigos de inicializacion
+        // Codidigo de inicializacion
         Parse.initialize(this,"7w5XCpgdU1WmdktjpSgM2cuYMYrRdCtm70j3odNz",
                 "0q860t7vxPtNEPcfwUqY1F45n3BJpMVHEYSdJYu1");
-
+        
         ParseUser.enableAutomaticUser();
         ParseACL defaultACL = new ParseACL();
 
