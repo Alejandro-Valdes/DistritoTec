6ab1a1332dfed22676832631fdc68c62c31d36e2
eduardolozano1993 <eduardolozano1993@gmail.com>
2015-03-30 18:20:56 -0600

    Added GNU license

--- Androides_PROYECTO_DistritoTec/app/src/main/java/itesm/mx/androides_proyecto_distritotec/MainActivity.java
+++ Androides_PROYECTO_DistritoTec/app/src/main/java/itesm/mx/androides_proyecto_distritotec/MainActivity.java
@@ -1,5 +1,4 @@
 package itesm.mx.androides_proyecto_distritotec;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -12,6 +11,34 @@ import com.parse.ParseUser;
 import itesm.mx.androides_proyecto_distritotec.LoginSingup.LoginSignupActivity;
 import itesm.mx.androides_proyecto_distritotec.MenuOpcionesTransporte.OpcionTransporte;
 
+/*  DistritoTEC
+	Copyright (C) 2014 - ITESM
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Authors:
+
+   ITESM representatives
+	Ing. Martha Sordia Salinas <msordia@itesm.mx>
+    Ing. Mario de la Fuente <mario.delafuente@itesm.mx>
+
+   ITESM students
+	Jose Eduardo Elizondo Lozano eduardolozano1993@gmail.com
+	Jesús Alejandro Valdés Valdés alejandro.valdesval@gmail.com
+	Oliver Alejandro Martínez Quiroz oliver.mtz10@gmail.com
+ */
 /**
  * MainActivity
  *
